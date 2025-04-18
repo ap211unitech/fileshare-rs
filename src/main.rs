73fc4026e65e81760227448cef2dd055ddf0b174
Arjun Porwal <porwalarjun95@gmail.com>
2025-04-18 22:02:04 +0530

    feat: initalized axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(|| async { "Healthy!" }));
+
+    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+
+    println!("Server started on: {} 🚀", listener.local_addr().unwrap());
+
+    axum::serve(listener, router)
+        .await
+        .expect("Error serving application!");
 }
