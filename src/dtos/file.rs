3f78243ccbe64a919b6c1f1b0b2dfe3051abb8ce
Arjun Porwal <porwalarjun95@gmail.com>
2025-04-24 22:41:08 +0530

    fix: correct validation function name for expires_at field in UploadFileRequest

--- src/dtos/file.rs
+++ src/dtos/file.rs
@@ -7,14 +7,14 @@ pub struct UploadFileRequest {
     #[validate(length(min = 1, message = "Name cannot be empty"))]
     pub file_name: String,
 
-    #[validate(custom(function = "validate_future_datetime"))]
+    #[validate(custom(function = "validate_expires_at"))]
     pub expires_at: DateTime<Utc>,
 
     #[validate(range(exclusive_min = 0, max = 10))]
     pub max_downloads: u128,
 }
 
-fn validate_future_datetime(date: &DateTime<Utc>) -> Result<(), ValidationError> {
+fn validate_expires_at(date: &DateTime<Utc>) -> Result<(), ValidationError> {
     if *date <= Utc::now() {
         return Err(ValidationError::new("`expires_at` must_be_in_future"));
     }
